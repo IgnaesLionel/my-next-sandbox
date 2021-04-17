a3e104b761a31319fad4844d958100cba61a2849
ignaesLionel <ignaes_lionel@hotmail.com>
2021-04-17 11:18:37 +0200

    clean code

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import ButtonArrow from "../components/ButtonArrow/ButtonArrow";
 import Card from "../ui/Card";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -9,33 +10,34 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}></main>
-      <Card>
-        <p>
-          {" "}
-          Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit
-          amet, consectetur adipLorem ipsum dolor sit amet, consectetur
-          adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit
-          amet, consectetur adipLorem ipsum dolor sit amet, consectetur
-          adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit
-          amet, consectetur adipLorem ipsum dolor sit amet, consectetur
-          adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit
-          amet, consectetur adipLorem ipsum dolor sit amet, consectetur
-          adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit
-          amet, consectetur adipLorem ipsum dolor sit amet, consectetur
-          adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit
-          amet, consectetur adipLorem ipsum dolor sit amet, consectetur
-          adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit
-          amet, consectetur adipLorem ipsum dolor sit amet, consectetur
-          adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit
-          amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip
-        </p>
-        <ButtonArrow buttonName="Clickez ici" />{" "}
-        <ButtonArrow buttonName="Clickez ici" />
-        <ButtonArrow buttonName="Clickez ici" />
-      </Card>
+
       <footer className={styles.footer}>
+        <Card>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit
+            amet, consectetur adipLorem ipsum dolor sit amet, consectetur
+            adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor
+            sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur
+            adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor
+            sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur
+            adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor
+            sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur
+            adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor
+            sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur
+            adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor
+            sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur
+            adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor
+            sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur
+            adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor
+            sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur
+            adip
+          </p>
+          <ButtonArrow buttonName="Clickez ici" />{" "}
+          <ButtonArrow buttonName="Clickez ici" />
+          <ButtonArrow buttonName="Clickez ici" />
+        </Card>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
